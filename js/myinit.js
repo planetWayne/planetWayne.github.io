525718591bc8a0b7e872e39e1a7d494f4b7e68d8
planetWayne <2609566+planetWayne@users.noreply.github.com>
2019-02-07 23:44:29 +0000

    card testing and index update

--- js/myinit.js
+++ js/myinit.js
@@ -2,4 +2,5 @@ $(document).ready(function(){
   $('.dropdown-button').dropdown();
   $(".dropdown-trigger").dropdown();
   $('.fixed-action-btn').floatingActionButton();
+  $('.tooltipped').tooltip();
 });
