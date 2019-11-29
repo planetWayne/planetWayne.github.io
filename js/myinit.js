03ead97f81356c318f47fd96ad42adbc5a8e4e31
Wayne <2609566+planetWayne@users.noreply.github.com>
2019-11-29 10:38:32 +0000

    boxed test

--- js/myinit.js
+++ js/myinit.js
@@ -4,5 +4,5 @@ $(document).ready(function(){
   $('.fixed-action-btn').floatingActionButton();
   $('.tooltipped').tooltip();
   $('.parallax').parallax();
-
+  $('.materialboxed').materialbox();
 });
